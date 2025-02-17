26709fe234db3cc659a9df50872ea1baf1c4c9b0
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-02-16 18:03:36 -1000

    Update nlp.c

--- nlp.c
+++ nlp.c
@@ -1,1 +1,11 @@
-Write a C program that processes a sentence character by character using a function pipeline.
+/*
+  Write a C program that processes a sentence character by character using a function pipeline.
+
+  You must:
+  - remove stopwords
+  - count the words in the processed sentence
+  - count vowels, uppercase and lowercase letters
+  - make sure the function calls are correct
+
+  ** To deal with string values and functions (when checking for stopwords), use string.h header file
+*/
